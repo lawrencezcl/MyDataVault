4095e59daeba14f7f9b7592eb380439d0bc2ad73
lawrencezcl <lawrencezcl@gmail.com>
2025-11-15 11:49:19 +0800

    Clean up conflicting API files and fix deployment conflicts

--- api/test.js
+++
@@ -1,9 +0,0 @@
-// This is a Vercel serverless function
-module.exports = (req, res) => {
-  const { name = 'World' } = req.query;
-  res.json({
-    message: `Hello ${name}!`,
-    timestamp: new Date().toISOString(),
-    endpoint: '/api/test'
-  });
-};
